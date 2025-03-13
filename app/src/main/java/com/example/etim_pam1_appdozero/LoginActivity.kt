caaaaa783fafdff15aed01ee4107e7fba2365776
Raul Luis <raulluispcastro@gmail.com>
2025-03-13 15:19:28 -0300

    ATT

--- app/src/main/java/com/example/etim_pam1_appdozero/LoginActivity.kt
+++ app/src/main/java/com/example/etim_pam1_appdozero/LoginActivity.kt
@@ -2,9 +2,15 @@ package com.example.etim_pam1_appdozero
 
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Button
+import android.widget.EditText
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity : AppCompatActivity() {
+    private  lateinit var email:EditText
+    private  lateinit var senha:EditText
+    private  lateinit var entrar:Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
